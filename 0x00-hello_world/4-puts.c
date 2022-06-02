e86af72080aa88e3addd481957dbca1549946c88
Vincent Ikechukwu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2022-06-02 10:17:19 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main function to print the text
+ * 
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
