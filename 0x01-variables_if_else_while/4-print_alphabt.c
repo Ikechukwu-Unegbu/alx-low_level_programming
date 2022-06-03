ee91a9ab527cbd3131895ddad9b6b9e25f2240f2
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-03 19:21:38 +0100

    M

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * main - prints the alphabet in lowercase,followed by a new line, except q and e
+ * Return: 0
  */
 int main(void)
 {
