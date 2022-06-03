8aafcf6846b7a3507ad6fe0d920ca02e2693a369
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-03 19:00:27 +0100

    M

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function body, prints alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char = alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
