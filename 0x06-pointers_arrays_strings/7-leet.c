8a27043ff0a0d7e7a372475fef96c163b1f188af
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-16 17:31:37 +0100

    4 5 6 tasks doen

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - entry point into the code
+ * @n: input variable
+ * Return: Always 0 (Success)
+ */
+char *leet(char *n)
+{
+	int i, x;
+	int fnd[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int rplcer[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (n[i] == fnd[x])
+			{
+				n[i] = rplcer[x / 2];
+				x = 9;
+			}
+		}
+	}
+
+	return (n);
+}
