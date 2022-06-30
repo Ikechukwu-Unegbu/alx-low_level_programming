c2315b80691c81e76bf001a54f0e813a75376446
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-30 23:51:30 +0100

    more on malloc and free tasks done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,47 +11,39 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1 = 0, size2 = 0, i;
+	unsigned int size0 = 0, size1 = 0, i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
+
+	while (s1[size0] != '\0')
+	{
+		size0++;
 	}
 
-	while (s1[size1] != '\0')
+	while (s2[size1] != '\0')
 	{
 		size1++;
 	}
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
-
-	if (n > size2)
-	{
-		n = size2;
-		p = malloc((size1 + n + 1) * sizeof(char));
-	}
+	if (n > size1)
+	n = size1;
+	p = malloc((size0 + n + 1) * sizeof(char));
 
 	if (p == NULL)
-	{
 		return (0);
-	}
 
-	for (i = 0; i < size1; i++)
+	for (i = 0; i < size0; i++)
 	{
 		p[i] = s1[i];
 	}
 
-	for (; i < (size1 + n); i++)
+	for (; i < (size0 + n); i++)
 	{
-		p[i] = s2[i - size1];
+		p[i] = s2[i - size0];
 	}
 	p[i] = '\0';
 
