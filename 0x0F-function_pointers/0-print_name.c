1646d2673616f498dc8c0ab58093b862eba82420
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-06 08:53:30 +0100

    task zero done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that takes and name and pointer to 
+ * a printing function and prints the name
+ * @name: pointer to a name to print
+ * @f: pointer to a printing function
+ * 
+ */
+void print_name(char *name, void (*f)(char*))
+{
+    if(!name || !f)
+    {
+        return;
+    }
+    f(name);
+}
