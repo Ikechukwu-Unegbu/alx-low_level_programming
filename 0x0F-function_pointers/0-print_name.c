1fc6783f83315f84090f7aca56c15d94f6dcc131
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-07 00:45:11 +0100

    fixing betty bug

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,11 @@
  * print_name - function that takes and name and pointer to 
  * a printing function and prints the name
  * @name: pointer to a name to print
- * @f: pointer to a printing function
- * 
+ * @f: pointer to a printing function 
  */
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
-    if( !name || !f)
-        return;
-    f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
