120de3825299f1ccbfb97fce6b7b5b7c618041aa
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-08 00:38:16 +0100

    fixing task zero

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,29 +1,24 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function that prints numbers.
- * @separator: separator string
- * @n: number of integers passed.
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
  * Return: no return.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int number;
-
-   
+	unsigned int i;
 
 	va_start(valist, n);
 
-	for (number = 0; number < n; number++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		if (separator && number < n - 1)
-        {
-            printf("%s", separator);
-        }
-
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
 
 	printf("\n");
