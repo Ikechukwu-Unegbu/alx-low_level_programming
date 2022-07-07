11b21104eca14b4816334feeb49db8dc6246e9c2
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-07 11:08:44 +0100

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,21 +5,17 @@
  * array_iterator - the function to iterate through an array
  * @array: input array
  * @size: array size
- * @action: function to act on the array items
- * 
+ * @action: function to act on the array items  
  */
 
-void array_iterator(int *array, size_t size,void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int x;
+	unsigned int x;
 
-    if(!array || !action)
-    {
-        return;
-    }
-    
-    for(x = 0; x < size; x++)
-    {
-        action(array[x]);
-    }
+	if (!array || !action)
+		return;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
 }
+
