26f58af8117771232ecb553e3eefb37d665fab84
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-08 00:31:36 +0100

    fixing task zero

--- 0x10-variadic_functions/0-Sum_them_all.c
+++ 0x10-variadic_functions/0-Sum_them_all.c
@@ -1,9 +1,10 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
- * Return: sum of its parameters or zero.
+ *
+ * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
