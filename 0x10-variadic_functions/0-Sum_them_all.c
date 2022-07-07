32913fa6bd7e594bba74de4f6e1d11ae8c258458
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-07 11:43:37 +0100

    task zero done

---
+++ 0x10-variadic_functions/0-Sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns sum of all the supplied variables
+ * @n: the number of variables supplied.
+ * @...: variable spread
+ * Return: zero or sum of all supplied parameters.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list numbers;
+    unsigned int i, sum =0;
+
+    va_start(numbers, n);
+
+    for(i = 0; i < n; i++)
+    {
+
+        sum += va_arg(numbers, int);
+
+    }
+
+    va_end(numbers);
+
+    return (numbers);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Introduction to Variadic Functions in C
