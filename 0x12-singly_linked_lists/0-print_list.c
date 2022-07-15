4e7b24ae0fa357021317274a4841c4cb7d1b2289
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-15 06:52:28 +0100

    task two done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,9 +4,10 @@
  * print_list - prints the elements of a linked list
  * @h: singly linked list.
  *Return: number of elements in the list.
- */
+*/
 
-size_t print_list(const list_t *h){
+size_t print_list(const list_t *h)
+{
     size_t lem;
 
     lem = 0;
